cf3985e3144af27d0e5027af1f850cd7c64260aa
Madneep-Rahar <dev06@waysaheadglobal.com>
2024-09-25 13:45:13 +0530

    server listen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -60,63 +60,67 @@ export const io = new Server(httpServer, {
 // });
 
 io.on('connection', async (socket) => {
-    console.log('a user connected', socket.handshake.auth.userId);
+    console.log("connections called", socket)
+})
+
+// io.on('connection', async (socket) => {
+//     console.log('a user connected', socket.handshake.auth.userId);
     
-    socket.join(socket.handshake.auth.userId);
-    await db.promise().query('UPDATE users SET is_online = 1 WHERE id = ?', [socket.handshake.auth.userId]);
+//     socket.join(socket.handshake.auth.userId);
+//     await db.promise().query('UPDATE users SET is_online = 1 WHERE id = ?', [socket.handshake.auth.userId]);
 
-    socket.on('disconnect', async () => {
-        console.log('user disconnected');
-        await db.promise().query('UPDATE users SET is_online = 0 WHERE id = ?', [socket.handshake.auth.userId]);
-    });
+//     socket.on('disconnect', async () => {
+//         console.log('user disconnected');
+//         await db.promise().query('UPDATE users SET is_online = 0 WHERE id = ?', [socket.handshake.auth.userId]);
+//     });
 
-    socket.on('join-room', (roomId) => {
-        socket.join(roomId);
-        console.log('user joined room:', roomId);
-    });
+//     socket.on('join-room', (roomId) => {
+//         socket.join(roomId);
+//         console.log('user joined room:', roomId);
+//     });
 
-    socket.on("leave-room", (roomId) => {
-        socket.leave(roomId);
-        console.log('user left room:', roomId);
-    });
+//     socket.on("leave-room", (roomId) => {
+//         socket.leave(roomId);
+//         console.log('user left room:', roomId);
+//     });
 
-    socket.on('send-message', async ({ roomId, message, sentAt, type, recepientId }) => {
-        const senderId = socket.handshake.auth.userId;
-        console.log({ roomId, message, sentAt, type, recepientId });
-        db.query('INSERT INTO dncm_messages (type, version, conversation_id, sender_id, body, sent_at, updated_at, deleted_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [type, 1, roomId, senderId, message, Date.now(), Date.now(), 0], (err, results) => {
-            if (err) {
-                console.log('Error sending message:', err);
-                return;
-            }
+//     socket.on('send-message', async ({ roomId, message, sentAt, type, recepientId }) => {
+//         const senderId = socket.handshake.auth.userId;
+//         console.log({ roomId, message, sentAt, type, recepientId });
+//         db.query('INSERT INTO dncm_messages (type, version, conversation_id, sender_id, body, sent_at, updated_at, deleted_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [type, 1, roomId, senderId, message, Date.now(), Date.now(), 0], (err, results) => {
+//             if (err) {
+//                 console.log('Error sending message:', err);
+//                 return;
+//             }
 
-            socket.emit('fetch-messages', { fetch: true });
-            io.to(recepientId).emit('fetch-messages', { fetch: true });
-            socket.to(roomId).emit('receive-message', message);
-        });
+//             socket.emit('fetch-messages', { fetch: true });
+//             io.to(recepientId).emit('fetch-messages', { fetch: true });
+//             socket.to(roomId).emit('receive-message', message);
+//         });
 
-        try {
-            const unsubscribeGroup = await getUnsubscribedGroups(recepientId);
-            const [data] = await db.promise().query<RowDataPacket[]>('SELECT is_online FROM users WHERE id = ?', [recepientId]);
-            const [blocks] = await db.promise().query<RowDataPacket[]>('SELECT id FROM blocks WHERE user_id = ? AND person_id = ?', [recepientId, senderId]);
-            const [reports] = await db.promise().query<RowDataPacket[]>('SELECT id FROM reports WHERE user_id = ? AND person_id = ?', [recepientId, senderId]);
+//         try {
+//             const unsubscribeGroup = await getUnsubscribedGroups(recepientId);
+//             const [data] = await db.promise().query<RowDataPacket[]>('SELECT is_online FROM users WHERE id = ?', [recepientId]);
+//             const [blocks] = await db.promise().query<RowDataPacket[]>('SELECT id FROM blocks WHERE user_id = ? AND person_id = ?', [recepientId, senderId]);
+//             const [reports] = await db.promise().query<RowDataPacket[]>('SELECT id FROM reports WHERE user_id = ? AND person_id = ?', [recepientId, senderId]);
 
 
-            if (!data[0].is_online && !unsubscribeGroup.includes(UnsubscribeGroup.MESSAGES) && !blocks.length && !reports.length) {
-                try {
-                    const [user] = await db.promise().query<RowDataPacket[]>('SELECT first_name, email FROM user_profiles WHERE user_id = ?', [recepientId]);
+//             if (!data[0].is_online && !unsubscribeGroup.includes(UnsubscribeGroup.MESSAGES) && !blocks.length && !reports.length) {
+//                 try {
+//                     const [user] = await db.promise().query<RowDataPacket[]>('SELECT first_name, email FROM user_profiles WHERE user_id = ?', [recepientId]);
 
-                    const [me] = await db.promise().query<RowDataPacket[]>('SELECT first_name FROM user_profiles WHERE user_id = ?', [socket.handshake.auth.userId]);
+//                     const [me] = await db.promise().query<RowDataPacket[]>('SELECT first_name FROM user_profiles WHERE user_id = ?', [socket.handshake.auth.userId]);
 
-                    await mailService.sendMessageMail(user[0].email, me[0].first_name);
-                } catch (error) {
-                    console.log('Error sending message mail:', error);
-                }
-            }
-        } catch (error) {
-            console.log('Error sending message mail:', error);
-        }
-    });
-});
+//                     await mailService.sendMessageMail(user[0].email, me[0].first_name);
+//                 } catch (error) {
+//                     console.log('Error sending message mail:', error);
+//                 }
+//             }
+//         } catch (error) {
+//             console.log('Error sending message mail:', error);
+//         }
+//     });
+// });
 
 
 // httpServer.listen({
